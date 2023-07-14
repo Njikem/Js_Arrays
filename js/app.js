eccedfe06a15dfbf2e938a6229fec412fc432e64
Njikem <123434988+Njikem@users.noreply.github.com>
2023-07-14 00:23:11 -0600

    js arrays

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 let username = [`bright`, `john`, `nunti`, `calos`, `hanna`];
 let user_age = [23, 35, 29, 32, 41];
+
+
+let first_user_name = [`Mauby`, `Shone`, `Alex`, `Elvis`];
+let name = first_user_name[0];
+console.log(name);
+
+let last_user_name = [`Carlos`, `Anna`, `Rose`, `Gla`];
+let user_name = last_user_name[3];
+console.log(username);
+
+let middle_user_age = [35,25, 39, 40, 45];
+let age = middle_user_age[3];
+console.log(age);
+
